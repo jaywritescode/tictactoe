8e6100b4b39d4e5d89e55011d0b97f955cd01b0d
Jay Harris <jaywritescode@users.noreply.github.com>
2018-02-11 15:47:49 -0500

    added BoardCreator

---
+++ src/test/java/info/jayharris/tictactoe/BoardCreator.java
@@ -0,0 +1,22 @@
+package info.jayharris.tictactoe;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class BoardCreator {
+
+    public static Board create(Piece[] pieces) {
+        return create(pieces, pieces.length);
+    }
+
+    public static Board create(Piece[] pieces, int size) {
+        Board board = new Board();
+
+        IntStream.range(0, size)
+                .filter(it -> pieces[it] != null)
+                .forEach(it -> board.setPiece(it, pieces[it]));
+        return board;
+    }
+}
