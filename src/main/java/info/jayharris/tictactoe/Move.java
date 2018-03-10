8122100427145d765016b12f7134827448fcfb18
Jay Harris <jaywritescode@users.noreply.github.com>
2018-03-09 20:37:52 -0500

    added equals and hashCode to Move

--- src/main/java/info/jayharris/tictactoe/Move.java
+++ src/main/java/info/jayharris/tictactoe/Move.java
@@ -2,6 +2,8 @@ package info.jayharris.tictactoe;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Objects;
+
 /**
  * References a square on the board that a player can play at.
  */
@@ -19,6 +21,19 @@ public class Move {
 
     public int getIndex() {
         return index;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Move move = (Move) o;
+        return index == move.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index);
     }
 
     /**
