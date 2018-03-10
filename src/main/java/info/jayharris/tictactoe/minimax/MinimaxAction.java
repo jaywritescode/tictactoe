5feb069355fd662e830d758bd8a1ce095120b30f
Jay Harris <jaywritescode@users.noreply.github.com>
2018-03-10 12:41:19 -0500

    add MinimaxAction

---
+++ src/main/java/info/jayharris/tictactoe/minimax/MinimaxAction.java
@@ -0,0 +1,41 @@
+package info.jayharris.tictactoe.minimax;
+
+import info.jayharris.minimax.Action;
+import info.jayharris.tictactoe.Board;
+import info.jayharris.tictactoe.Move;
+
+import java.util.Objects;
+
+public class MinimaxAction implements Action<MinimaxState> {
+
+    Move move;
+
+    MinimaxAction(int squareRef) {
+        this.move = Move.at(squareRef);
+    }
+
+    @Override
+    public MinimaxState apply(MinimaxState state) {
+        Board board = state.getBoardCopy();
+        board.setPiece(move, state.getToMove());
+
+        return new MinimaxState(board, state.getToMove().opposite());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MinimaxAction that = (MinimaxAction) o;
+        return Objects.equals(move, that.move);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(move);
+    }
+
+    public static MinimaxAction from(int squareRef) {
+        return new MinimaxAction(squareRef);
+    }
+}
