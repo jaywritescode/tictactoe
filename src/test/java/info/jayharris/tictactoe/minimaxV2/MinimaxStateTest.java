8117f6bcb7d1837d5771581ded15513d4478bc11
Jay Harris <jaywritescode@users.noreply.github.com>
2018-06-22 20:05:07 -0400

    test MinimaxState#actions

--- src/test/java/info/jayharris/tictactoe/minimaxV2/MinimaxStateTest.java
+++ src/test/java/info/jayharris/tictactoe/minimaxV2/MinimaxStateTest.java
@@ -1,5 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package info.jayharris.tictactoe.minimaxV2;
+
+
+import info.jayharris.tictactoe.BoardCreator;
+import info.jayharris.tictactoe.Move;
+import info.jayharris.tictactoe.Piece;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MinimaxStateTest {
 
+    @Test
+    @DisplayName("it gets all the legal actions")
+    void actions() {
+        MinimaxState state = MinimaxState.of(BoardCreator.create(new Piece[][] {
+                //@formatter:off
+                new Piece[] { null   , Piece.O, null },
+                new Piece[] { Piece.X, Piece.X, null },
+                new Piece[] {    null,    null, null }
+        }), Piece.O);
+
+        assertThat(state.actions())
+                .extracting("move")
+                .containsExactlyInAnyOrder(Move.at(0), Move.at(2), Move.at(5), Move.at(6), Move.at(7), Move.at(8));
+    }
 }
