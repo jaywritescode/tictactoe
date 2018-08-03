ec4be92382eac2933b6f80d907a1bdea2786df8f
Jay Harris <jaywritescode@users.noreply.github.com>
2018-08-02 21:17:37 -0400

    notes

--- src/main/java/info/jayharris/tictactoe/player/MinimaxPlayer.java
+++ src/main/java/info/jayharris/tictactoe/player/MinimaxPlayer.java
@@ -15,6 +15,9 @@ public class MinimaxPlayer extends Player {
 
     @Override
     public Move getMove(Tictactoe game) {
+        // The problem is that this creates a new minimax tree and
+        // recalculates every time we call this method, even though the
+        // tree has already been calculated.
         DecisionTree<MinimaxState, MinimaxAction> decisionTree = new DecisionTree<>(MinimaxState.root(game, this));
 
         return decisionTree.perform().getMove();
